e350ff8ccc062b2daf7825f2d5c277b0d4aeeae7
Vicente Vásquez <vicente.vasquez538@gmail.com>
2024-06-07 17:57:04 -0400

    change nemf implementation

---
+++ neural_cf/gmf.py
@@ -0,0 +1,46 @@
+import torch
+from engine import Engine
+from utils import use_cuda
+from torch import nn
+
+
+class GMF(torch.nn.Module):
+    def __init__(self, config):
+        super(GMF, self).__init__()
+        self.num_users = config['num_users']
+        self.num_items = config['num_items']
+        self.latent_dim = config['latent_dim']
+
+        self.embedding_user = torch.nn.Embedding(num_embeddings=self.num_users, embedding_dim=self.latent_dim)
+        self.embedding_item = torch.nn.Embedding(num_embeddings=self.num_items, embedding_dim=self.latent_dim)
+
+        self.affine_output = torch.nn.Linear(in_features=self.latent_dim, out_features=1)
+        self.logistic = torch.nn.Sigmoid()
+
+        # Initialize model parameters with a Gaussian distribution (with a mean of 0 and standard deviation of 0.01)
+        if config['weight_init_gaussian']:
+            for sm in self.modules():
+                if isinstance(sm, (nn.Embedding, nn.Linear)):
+                    print(sm)
+                    torch.nn.init.normal_(sm.weight.data, 0.0, 0.01)
+
+    def forward(self, user_indices, item_indices):
+        user_embedding = self.embedding_user(user_indices)
+        item_embedding = self.embedding_item(item_indices)
+        element_product = torch.mul(user_embedding, item_embedding)
+        logits = self.affine_output(element_product)
+        rating = self.logistic(logits)
+        return rating
+
+    def init_weight(self):
+        pass
+
+
+class GMFEngine(Engine):
+    """Engine for training & evaluating GMF model"""
+    def __init__(self, config):
+        self.model = GMF(config)
+        if config['use_cuda'] is True:
+            use_cuda(True, config['device_id'])
+            self.model.cuda()
+        super(GMFEngine, self).__init__(config)
